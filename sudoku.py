4387dec95e36d161f78b9edf35a5404eb4c927b7
Ronen <ronen.simanduyev1999@gmail.com>
2022-10-28 15:03:03 +0300

    Initial commit

---
+++ sudoku.py
@@ -0,0 +1,44 @@
+import numpy as np
+grid = [[5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,0,1,9,0,0,5],
+        [0,0,0,0,0,0,0,0,0]]
+
+def possible(row, column, number):
+    global grid
+    # check if the number is in the row
+    for i in range(9):
+        if grid[row][i] == number:
+            return False
+    # check if the number is in the column
+    for i in range(9):
+        if grid[i][column] == number:
+            return False
+    # check if the number is in the square
+    x=(column//3)*3
+    y=(row//3)*3
+    for i in range (3):
+        for j in range(3):
+            if grid[y+i][x+j] == number:
+                return False
+    return True
+
+def solve():
+    global grid
+    for row in range(9):
+        for column in range(9):
+            if grid[row][column] == 0:
+                for number in range (1,10):
+                    if possible(row, column,number):
+                        grid[row][column] = number
+                        solve()
+                        grid[row][column] = 0
+                return
+    print(np.matrix(grid),'\n')
+
+solve()
